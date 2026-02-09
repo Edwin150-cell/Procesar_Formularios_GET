4fe18f6d77ac64d34eefbb736c4a29e45637b92b
Edwin150-cell <winalonso488@gmail.com>
2026-02-08 22:51:37 -0600

    Add files via upload

---
+++ recibir.php
@@ -0,0 +1,10 @@
+<?
+    //peocasar formularios con ge
+    var_dump($_GET);
+    echo "<hr>";
+    $elNombre = $_GET['nombre']; // Get es un arreglo asociativo
+    echo $elNombre;
+
+    $edad = $_GET['edad'];
+    echo $edad;
+?>
